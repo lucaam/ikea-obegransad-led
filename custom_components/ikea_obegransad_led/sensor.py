5e76ab35d7f6a6a481b356e6ed5d9935fb044280
Luca Amoriello <luca.amoriello@hotmail.it>
2025-02-14 18:05:53 +0100

    Fix other sensors error

--- custom_components/ikea_obegransad_led/sensor.py
+++ custom_components/ikea_obegransad_led/sensor.py
@@ -1,18 +1,18 @@
 """Sensor platform for IKEA OBEGRÄNSAD Led."""
 
-from .const import DEFAULT_NAME
-from .const import DOMAIN
-from .const import ICON
-from .const import SENSOR
-from .entity import IkeaObegransadLedEntity
 import logging
 
+from .const import DEFAULT_NAME, DOMAIN, ICON, SENSOR
+from .entity import IkeaObegransadLedEntity
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
+    _LOGGER.debug(
+        "Setting up sensor platform for IKEA OBEGRÄNSAD Led."
+    )  # Log setup start
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_devices([IkeaObegransadLedSensor(coordinator, entry)])
 
@@ -28,7 +28,21 @@ class IkeaObegransadLedSensor(IkeaObegransadLedEntity):
     @property
     def state(self):
         """Return the state."""
-        return self.coordinator.data.get("brightness", 0)
+        if self.coordinator.data is None:  # Check if data is None
+            _LOGGER.debug(
+                "Coordinator data is None, returning None for state."
+            )  # Log the None case
+            return None  # Or return a default value like 0 if you prefer
+
+        brightness = self.coordinator.data.get("brightness")  # No default value here
+        if brightness is None:  # check if brightness is in the data
+            _LOGGER.debug(
+                "Brightness value not found in data, returning None for state."
+            )
+            return None
+
+        _LOGGER.debug("Current brightness: %s", brightness)  # Log brightness value
+        return brightness
 
     @property
     def icon(self):
